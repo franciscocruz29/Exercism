daf3cf6d0bf414f3a6a4a378677c13c29da49c02
franciscocruz29 <franciscohuertas25@gmail.com>
2023-03-15 10:30:15 -0500

    Completed RNA Transcription

---
+++ javascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,9 @@
+export const toRna = (dna) => {
+  const map = {
+    'G': 'C',
+    'C': 'G',
+    'T': 'A',
+    'A': 'U'
+  };
+  return dna.split('').map(nucleotide => map[nucleotide]).join('');
+};
