2d3b5aa5a817f10c0bfc0c49dd74f7791af9c193
franciscocruz29 <franciscohuertas25@gmail.com>
2023-03-15 10:05:30 -0500

    Modified pangram function

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,6 +1,10 @@
+from string import ascii_lowercase
+
+ALPHABET = set(ascii_lowercase)
+
 def is_pangram(sentence):
     """
     Return True if the sentence is a pangram, False otherwise.
     """
-    return set('abcdefghijklmnopqrstuvwxyz') <= set(sentence.lower())
+    return ALPHABET.issubset(sentence.lower())
 
