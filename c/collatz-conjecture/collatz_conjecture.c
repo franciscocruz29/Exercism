6776fd7a51bdb5712d97eef1bc08d284bc543c42
Francisco Cruz <franciscohuertas25@gmail.com>
2025-03-11 10:16:58 -0500

    Update collatz_conjecture.c

--- c/collatz-conjecture/collatz_conjecture.c
+++ c/collatz-conjecture/collatz_conjecture.c
@@ -7,15 +7,19 @@
 // The number of steps required to reach 1
 
 // What are the rules?
-// Take any positive integer n. If n is even, divide n by 2 to get n / 2. If n is odd, multiply n by 3 and add 1 to get 3n + 1. Repeat the process indefinitely.
-// The conjecture states that no matter which number you start with, you will always reach 1 eventually.
-// If n is not a positive integer, stop the program from being executed further and return an error message.
+// Take any positive integer n. If n is even, divide n by 2 to get n / 2. If n
+// is odd, multiply n by 3 and add 1 to get 3n + 1. Repeat the process
+// indefinitely. The conjecture states that no matter which number you start
+// with, you will always reach 1 eventually. If n is not a positive integer,
+// stop the program from being executed further and return an error message.
 
 // 2. Examples:
 
-// Starting witn n = 4, the steps would be as follows: 4, 2, 1. So for input n = 4, the return value would be 2.
-// Starting with n = 6, the steps would be as follows: 6, 3, 10, 5, 16, 8, 4, 2, 1. So for input n = 6, the return value would be 8.
-// Starting with n = 12, the steps would be as follows: 12, 6, 3, 10, 5, 16, 8, 4, 2, 1. So for input n = 12, the return value would be 9.
+// Starting witn n = 4, the steps would be as follows: 4, 2, 1. So for input n =
+// 4, the return value would be 2. Starting with n = 6, the steps would be as
+// follows: 6, 3, 10, 5, 16, 8, 4, 2, 1. So for input n = 6, the return value
+// would be 8. Starting with n = 12, the steps would be as follows: 12, 6, 3,
+// 10, 5, 16, 8, 4, 2, 1. So for input n = 12, the return value would be 9.
 
 // 3. Algorithm:
 
