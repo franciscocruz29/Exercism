a83d4b34307c8aed5a95cf702d8098e7f4140a0c
Francisco Cruz <franciscohuertas25@gmail.com>
2025-03-19 14:28:50 -0500

    Add PEDAC step-2 etl.js

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -17,7 +17,51 @@
 // * What is the mental model?
 //    * A reorganization of data that maintains the same information but in a format better suited for the game's expanded language support.
 
+
 // Step 2 - Examples:
+
+// Input: { 1: ["A"] }
+// Output: { a: 1 }
+
+// Input: {
+//    1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+//    2: ["D", "G"],
+//    3: ["B", "C", "M", "P"],
+//    4: ["F", "H", "V", "W", "Y"],
+//    5: ["K"],
+//    8: ["J", "X"],
+//    10: ["Q", "Z"],
+//  };
+
+// Output: {
+//    a: 1,
+//    b: 3,
+//    c: 3,
+//    d: 2,
+//    e: 1,
+//    f: 4,
+//    g: 2,
+//    h: 4,
+//    i: 1,
+//    j: 8,
+//    k: 5,
+//    l: 1,
+//    m: 3,
+//    n: 1,
+//    o: 1,
+//    p: 3,
+//    q: 10,
+//    r: 1,
+//    s: 1,
+//    t: 1,
+//    u: 1,
+//    v: 4,
+//    w: 4,
+//    x: 8,
+//    y: 4,
+//    z: 10,
+//  };
+
 
 // Step 3 - Strategies:
 
@@ -26,5 +70,5 @@
 // Step 5 - Implementation:
 
 export const transform = () => {
-  throw new Error("Remove this statement and implement this function");
+  throw new Error("Error");
 };
