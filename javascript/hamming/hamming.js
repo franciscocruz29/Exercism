bccfbda47af1d9017d56f8dcb113394a18a51aa5
Francisco Cruz <franciscohuertas25@gmail.com>
2025-04-09 10:24:29 -0500

    Add PEDAC step-1 hamming.js

---
+++ javascript/hamming/hamming.js
@@ -0,0 +1,22 @@
+// Step 1 - Problem Understanding
+//
+// * What are the expected inputs?
+//     * Data type: string
+//     * Description: DNA strands, using the letters 'A', 'C', 'G', and 'T'
+//
+// * What are the expected outputs?
+//     * Data type: number
+//     * Description: The number of differing positions (non-negative integer)
+//
+// * What are the key rules or constraints?
+//    - The DNA strands must be of equal length
+//    - The comparison is position-by-position (index-based)
+//
+// * What is the mental model?
+//    - Compare each character in the two strands and count the number of mismatches
+//
+// Step 5 - Implementation
+//
+export const compute = (strand_a, strand_b) => {
+  throw new Error("Remove this statement and implement this function");
+};
